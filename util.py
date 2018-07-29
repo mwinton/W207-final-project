ead6b69c2aa134602a521cab06ed7959494a7e77
Mike Winton <mwinton@ischool.berkeley.edu>
2018-07-28 22:26:09 -0700

    added imputation and OHE to util

--- util.py
+++ util.py
@@ -5,6 +5,9 @@ import numpy as np
 import pandas as pd
 from functools import partial
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import OneHotEncoder
+
 
 ### Cleanup utility functions
 
@@ -50,11 +53,69 @@ our_train_test_split = partial(train_test_split,
                                test_size=TEST_SIZE,
                                random_state=RANDOM_STATE)
 
+def ohe_data(train_data, test_data, factor_cols=['zip','district']):
+    '''
+        inputs: train_data, test_data (pandas dataframes)
+        returns: train_data_ohe, test_data_ohe (both sparse matrices)
+    '''
+    
+    # get indices for specified columns
+    factor_col_ids = []
+    for f in factor_cols:
+        idx = train_data.columns.get_loc(f) # column order assumed to be same in test set
+        factor_col_ids.append(idx)
+    factor_col_ids = np.array(factor_col_ids)
+
+    # perform one hot encoding
+    ohe_enc = OneHotEncoder(categorical_features=factor_col_ids, handle_unknown='ignore')
+    train_data_ohe = ohe_enc.fit_transform(train_data)
+    test_data_ohe = ohe_enc.transform(test_data)
+    print('Train data initial shape:',train_data.shape)
+    print('Test  data initial shape:',test_data.shape)
+    print('Train data new shape:',train_data_ohe.shape)
+    print('Test  data new shape:',test_data_ohe.shape)
+    
+    return train_data_ohe, test_data_ohe
+    
 def read_data(data_file='data_merged/combined_data_2018-07-27.csv'):
     merged_df = pd.read_csv(data_file)
-    X = merged_df.loc[:, ~merged_df.columns.isin(['high_registrations'])]
-    y = merged_df.loc[:, merged_df.columns.isin(['high_registrations'])]
-    train_data, test_data, train_labels, test_labels = our_train_test_split(X, y)
+    
+    # these columns cannot/should not be imputed
+    # notably, don't impute for `school_income_estimate` because too many missing values
+    non_impute_cols = ['dbn', 
+                       'school_name',
+                       'district',
+                       'zip',
+                       'school_income_estimate']
+    
+    # temporarily split out the non-numeric cols into a separate dataframe
+    tmp_non_numeric_df = merged_df[non_impute_cols]
+    tmp_numeric_df = merged_df.drop(non_impute_cols, axis=1)
+    
+    # do imputation of missing values to column mean
+    imp = Imputer(missing_values=np.nan, strategy='mean', axis=0)
+    tmp_imputed_df = pd.DataFrame(imp.fit_transform(tmp_numeric_df))
+    tmp_imputed_df.columns = tmp_numeric_df.columns
+    tmp_imputed_df.index = tmp_numeric_df.index
+    
+    # reassemble into a single dataframe
+    imputed_df = pd.concat([tmp_non_numeric_df, tmp_imputed_df], axis=1)
+
+    # split into features (X) and labels (y)
+    X = imputed_df.loc[:, ~imputed_df.columns.isin(['high_registrations'])]
+    y = imputed_df.loc[:, imputed_df.columns.isin(['high_registrations'])]
+    train_data, test_data, train_labels, test_labels = our_train_test_split(X, y, stratify=y)
+    
+    # convert y values into 1D array, as expected by sklearn classifiers
+    train_labels = train_labels.values.ravel()
+    test_labels = test_labels.values.ravel()
+    
+    # confirm stratification
+    print('Train: %d observations (positive class fraction: %.3f)' %
+          (len(train_labels), np.sum(train_labels==1) / len(train_labels)))
+    print('Test : %d observations (positive class fraction: %.3f)' % 
+          (len(test_labels), np.sum(test_labels==1) / len(test_labels)))
+
     return train_data, test_data, train_labels, test_labels
 
 def print_cv_results(cv_scores):
