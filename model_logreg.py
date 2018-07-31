e825b2364d044771c1fb9836562780fa5b6ccca8
Mike Winton <mwinton@ischool.berkeley.edu>
2018-07-31 00:16:13 -0700

    trying to help Daniel catch up after vacation

--- model_logreg.py
+++ model_logreg.py
@@ -6,13 +6,14 @@
 # 
 # ### Andrew Larimer, Deepak Nagaraj, Daniel Olmstead, Michael Winton (W207-4-Summer 2018 Final Project)
 
-# In[ ]:
+# In[3]:
 
 
 # import necessary libraries
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import util
 from util import our_train_test_split
 
 # set default options
@@ -22,17 +23,29 @@ pd.set_option('display.max_rows', 200)
 get_ipython().magic('matplotlib inline')
 
 
-# In[ ]:
+# ## Load data and split class labels into separate array
+# 
+# Our utility function reads the merged dataset, imputes the column mean for missing numeric values, and then performs a stratified train-test split.
+
+# In[7]:
 
 
-# Import cleaned dataset
-merged_df = pd.read_csv('data_merged/combined_data_2018-07-18.csv')
+# read the "production" version of the cleaned & merged dataset
+train_data_orig, test_data_orig, train_labels, test_labels = util.read_data(do_imputation=True)
+print(train_data_orig.shape)
+print(train_labels.shape)
 
-merged_df['number_enrolled'] = np.array((merged_df['num_shsat_test_takers']*100)/merged_df['pct_test_takers']).astype(int)
+
+# In[8]:
+
+
+train_data_orig['number_enrolled'] = np.array((train_data['num_shsat_test_takers']*100)/
+                                         train_data['pct_test_takers']).astype(int)
+test_data_orig['number_enrolled'] = np.array((test_data['num_shsat_test_takers']*100)/
+                                        test_data['pct_test_takers']).astype(int)
 
 # Keep the numeric columns.
 features_to_keep = [
-                    'high_registrations',
                     #'district', 
                     #'zip',
                     'community_school', 
@@ -97,58 +110,15 @@ features_to_keep = [
                     'number_enrolled'
                    ]
 
-X = merged_df[features_to_keep]
-X.head()
-
-
-# Split DataFrame into data and labels
-
-# In[ ]:
-
-
-y = X['high_registrations']
-X = X.drop(['high_registrations'], axis=1)
-
-
-# ## Deal with NaNs
-# Some columns have NaNs.  We'll try imputing these to the mean.
-
-# In[ ]:
-
-
-from sklearn.preprocessing import Imputer
-
-imp = Imputer(missing_values=np.nan, strategy='mean')
-X_i = pd.DataFrame(imp.fit_transform(X))
-X_i.columns = X.columns
-X_i.index = X.index
-X_i.head()
-
-
-# In[ ]:
-
-
-X_pos = X_i[y==1]
-X_neg = X_i[y==0]
-
-
-# ## Split the dataset into train and test splits
-
-# In[ ]:
-
-
-from functools import partial
-from sklearn.model_selection import train_test_split
-import util
-
-train_data, test_data, train_labels, test_labels = util.our_train_test_split(X_i, y, stratify = y)
+train_data = train_data_orig[features_to_keep]
+test_data = test_data_orig[features_to_keep]
 train_data.head()
 
 
 # ## Hyperparameter Tuning
 # Find the optimal C-score
 
-# In[ ]:
+# In[12]:
 
 
 from sklearn.preprocessing import StandardScaler
@@ -169,7 +139,7 @@ print('Best C:', best_model.best_estimator_.get_params()['C'])
 
 
 
-# In[ ]:
+# In[14]:
 
 
 from sklearn import metrics
@@ -182,7 +152,7 @@ for c in c_values['C']:
     train_data_scaled = scaler.fit_transform(train_data)
     test_data_scaled = scaler.transform(test_data)
     log = LogisticRegression(C=c, penalty='l2', random_state=207)
-    log.fit(train_data_scaled, train_labels.values.ravel())
+    log.fit(train_data_scaled, train_labels)
     log_predicted_labels = log.predict(test_data_scaled)
     log_f1 = metrics.f1_score(test_labels, log_predicted_labels, average='weighted')
     log_accuracy = metrics.accuracy_score(test_labels, log_predicted_labels)
@@ -196,39 +166,39 @@ print("\nThe best C value is {} with an F1 of {:.4f}".format(best_c, top_f1))
 
 # ## Make Pipeline and K-fold validation
 
-# In[ ]:
+# In[15]:
 
 
 from sklearn.model_selection import cross_val_score, cross_validate
 from sklearn.pipeline import make_pipeline
 
 pipe = make_pipeline(StandardScaler(), LogisticRegression(C=.1, penalty='l1', random_state=207))
-k_folds = 10
+k_folds = 5
 cv_scores = cross_validate(pipe, train_data, train_labels, cv=k_folds, scoring=['accuracy','f1'])
-
-# display accuracy with 95% confidence interval
-cv_accuracy = cv_scores['test_accuracy']
-print ('With %d-fold cross-validation, accuracy is: %.3f (95%% CI from %.3f to %.3f).' %
-       (k_folds, cv_accuracy.mean(), cv_accuracy.mean() - 1.96 * cv_accuracy.std(),
-        cv_accuracy.mean() + 1.96 * cv_accuracy.std()))
-
-# display F1 score with 95% confidence interval
-cv_f1 = cv_scores['test_f1']
-print ('The F1 score is: %.3f (95%% CI from %.3f to %.3f).' %
-       (cv_f1.mean(), cv_f1.mean() - 1.96 * cv_f1.std(),
-        cv_f1.mean() + 1.96 * cv_f1.std()))
+util.print_cv_results(cv_scores)
 
 
 # ## Examine coefficients
 
-# In[ ]:
+# In[157]:
+
+
+# recombine train and test data into an aggregate dataset
+X_orig = pd.concat([train_data_orig, test_data_orig])
+X_i = pd.concat([train_data, test_data])
+y = np.concatenate((train_labels,test_labels))
+X_pos = X_i[y==1]
+X_neg = X_i[y==0]
+
+
+# In[158]:
 
 
 from sklearn.model_selection import RepeatedStratifiedKFold
 
 # Run coefficient analysis on 100% of the data
 np_train_data = np.array(scaler.fit_transform(X_i))
-np_train_labels = np.array(y)
+np_train_labels = y
 
 # Run k-fold cross-validation with 5 folds 10 times, which means every school is predicted 10 times.
 folds = 5
@@ -240,7 +210,7 @@ fold_list = []
 for f in range(1, (folds*repeats)+1):
     fold_list.append('k{}'.format(f))
 coefs = pd.DataFrame(index=train_data.columns, columns=fold_list)
-predictions = pd.DataFrame(index=merged_df.index, columns = fold_list)
+predictions = pd.DataFrame(index=X_i.index, columns = fold_list)
 
 # Iterate through the Repeated Stratified K Fold, and and fill out the DataFrames
 counter = 1
@@ -261,7 +231,7 @@ sorted_coefs
 # ## Distributions
 # ### Most positively-influential features
 
-# In[ ]:
+# In[160]:
 
 
 # Get the top and bottom 5 most influential coefficients
@@ -278,7 +248,7 @@ plt.show()
 
 # ### Most negatively-influential features
 
-# In[ ]:
+# In[161]:
 
 
 fig = plt.figure(figsize=(20,60))
@@ -294,7 +264,7 @@ plt.show()
 # ## Examining Wrong Answers
 # We can build a table of all the kfold predictions, and see the degree to which the model got each school right or wrong
 
-# In[ ]:
+# In[162]:
 
 
 import seaborn as sns
@@ -302,7 +272,7 @@ sns.regplot(X_pos.grade_7_math_4s_hispanic_or_latino, X_pos.grade_7_ela_4s_hispa
 sns.regplot(X_neg.grade_7_math_4s_hispanic_or_latino, X_neg.grade_7_ela_4s_hispanic_or_latino)
 
 
-# In[ ]:
+# In[163]:
 
 
 fig, ax = plt.subplots(figsize=(10,10))
@@ -311,7 +281,7 @@ ax = sns.regplot(X_i.grade_7_ela_4s_hispanic_or_latino, y, logistic=True, label=
 ax.legend()
 
 
-# In[ ]:
+# In[164]:
 
 
 fig, ax = plt.subplots(figsize=(10,10))
@@ -319,7 +289,7 @@ ax = sns.regplot(X_i.number_enrolled, y, logistic=True, label="Number Enrolled")
 ax.legend()
 
 
-# In[ ]:
+# In[44]:
 
 
 fig, ax = plt.subplots(figsize=(10,10))
@@ -330,7 +300,7 @@ ax.set_ylabel('Grade 7 ELA 4s, Hispanic or Latino', fontsize=15)
 plt.show
 
 
-# In[ ]:
+# In[165]:
 
 
 predictions['1s'] = predictions.iloc[:,:50].sum(axis=1)
@@ -338,17 +308,16 @@ predictions['0s'] = (predictions.iloc[:,:50]==0).sum(axis=1)
 predictions['true'] = y
 
 
-# In[ ]:
+# In[166]:
 
 
 # Create a table of raw results, the vote for truth
-X_predicted = pd.concat([X_i, predictions['1s'], predictions['0s'], y], axis=1, join_axes=[X_i.index])
+X_predicted = pd.concat([X_i, predictions['1s'], predictions['0s'], pd.DataFrame(y)], axis=1, join_axes=[X_i.index])
 X_predicted = X_predicted.sort_values(by=['1s', '0s'], ascending=[False, True])
-
 
 scaled_X_predicted = scaler.fit_transform(X_predicted)
 avg_coefs = np.array(coefs['avg'])
-weighted_values = np.multiply(scaled_X_predicted, avg_coefs)
+weighted_values = np.multiply(scaled_X_predicted[:, :-3], avg_coefs)
 X_result_weighted = X_predicted.copy()
 X_result_weighted.iloc[:, :-3] = weighted_values
 X_result_weighted = X_result_weighted.iloc[:, :]
@@ -360,7 +329,7 @@ X_result_weighted_trimmed = X_result_weighted.drop(drop_cols, axis=1)
 X_result_weighted_trimmed.head()
 
 
-# In[ ]:
+# In[167]:
 
 
 fig, ax = plt.subplots(figsize=(18,200))
@@ -381,17 +350,19 @@ plt.show()
 
 # It is perhaps most useful to examine the false positives - that is, schools that did NOT have high SHSAT registrations, but that the model thought SHOULD have.  We'll put the threshhold at 5 or more incorrect "true" classifications, and rank them in descending order (ie, the schools the model got most consistently wrong at the top).
 
-# In[ ]:
+# In[168]:
 
 
 false_positives = predictions[predictions['true']==0]
 false_positives = false_positives[false_positives['1s'] > 5].sort_values(by='1s', ascending=False)['1s']
-false_positives
-fp_result = pd.concat([false_positives, merged_df.iloc[false_positives.index]], axis=1, join_axes=[false_positives.index])
+# false_positives
+
+# MW: join wasn't working properly; I fixed it (and verified a few rows)
+fp_result = pd.concat([false_positives, X_orig], axis=1, join='inner')
 fp_result
 
 
-# In[ ]:
+# In[171]:
 
 
 # Just the columns of interest
@@ -408,7 +379,7 @@ df_pct = np.multiply(fp_result[pct_features], .01)
 df_false_positives = pd.concat([fp_result[fp_features], df_pct], axis=1)
 
 # Determine the number of test takers this school would have needed to meet the median percentage of high_registrations
-median_pct = np.median(merged_df[merged_df['high_registrations']==1]['pct_test_takers'])/100
+median_pct = np.median(X_orig[y==1]['pct_test_takers'])/100
 predicted_test_takers = np.multiply(df_false_positives['number_enrolled'], median_pct)
 
 # Subtract the number of actual test takers from the hypothetical minimum number
@@ -429,7 +400,9 @@ df_false_positives
 
 # Determine p-value for each value in the false positives group
 
-# In[ ]:
+# ## MW: I stopped updating here.  I think there might have already been a bug in your code for p-values.
+
+# In[172]:
 
 
 scaled_fp_X = scaler.fit_transform(fp_result.iloc[:,1:])
